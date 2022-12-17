f96f00fccb216206186a487436352188642b780c
Thorhian <thorhian@gmail.com>
2022-12-16 19:20:43 -0700

    Points are sorted by default now

--- src/main.rs
+++ src/main.rs
@@ -16,28 +16,18 @@ fn find_rectangle_points(
     let point2 = Vector2::new(-norm_rad1[1], norm_rad1[0]) + center2;
     let point3 = Vector2::new(norm_rad2[1], -norm_rad2[0]) + center1;
     let point4 = Vector2::new(-norm_rad2[1], norm_rad2[0]) + center1;
+    let mut points = vec![point1, point2, point3, point4];
+    points.sort_unstable_by(|p1, p2|{
+        let mut p1_rad = p1[1].atan2(p1[0]);
+        let mut p2_rad = p2[1].atan2(p2[0]);
 
-    Matrix2x4::from_columns(&[point1, point2, point3, point4])
-}
+        if p1_rad < 0.0 { p1_rad += std::f32::consts::PI * 2.0 }
+        if p2_rad < 0.0 { p2_rad += std::f32::consts::PI * 2.0 }
 
-fn sort_rectangle_points(points: Matrix2x4<f32>) {
-    let mut avg_center: Vector2<f32> = Vector2::zeros();
-    let mut polar_coords: Vec<f32> = Vec::new();
+        p1_rad.partial_cmp(&p2_rad).unwrap()
+    });
 
-    for col in points.column_iter() {
-        avg_center += col;
-        let mut polar_rot = col[1].atan2(col[0]);
-        if polar_rot < 0.0  {
-            polar_rot += std::f32::consts::PI * 2.0;
-        }
-        polar_coords.push(polar_rot);
-    }
-
-    avg_center = avg_center / 4.0;
-    println!("Average Center: {}", avg_center);
-    
-
-    
+    Matrix2x4::from_columns(&points)
 }
 
 fn main() {
@@ -45,5 +35,5 @@ fn main() {
     let center2 = Vector2::new(1.0, 2.0);
     let radius = 6.0 / 0.2;
     let quad = find_rectangle_points(center1, center2, radius);
-    sort_rectangle_points(quad);
+    println!("Sorted Quad: {}", quad);
 }
