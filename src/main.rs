edd10e4f3d725fa0911ff43e038751cebf8a7f71
Thorhian <thorhian@gmail.com>
2022-12-17 09:25:52 -0700

    Created compute_worker module

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,10 @@
-use std;
-use nalgebra::Matrix2x4;
 use nalgebra::Vector2;
-
-fn find_rectangle_points(
-    center1: Vector2<f32>,
-    center2: Vector2<f32>,
-    radius: f32,
-) -> Matrix2x4<f32> {
-    let translated_cent1 = center1 - center2;
-    let translated_cent2 = center2 - center1;
-    let norm_rad1 = Vector2::normalize(&translated_cent1) * radius;
-    let norm_rad2 = Vector2::normalize(&translated_cent2) * radius;
-
-    let point1 = Vector2::new(norm_rad1[1], -norm_rad1[0]) + center2;
-    let point2 = Vector2::new(-norm_rad1[1], norm_rad1[0]) + center2;
-    let point3 = Vector2::new(norm_rad2[1], -norm_rad2[0]) + center1;
-    let point4 = Vector2::new(-norm_rad2[1], norm_rad2[0]) + center1;
-    let mut points = vec![point1, point2, point3, point4];
-    points.sort_unstable_by(|p1, p2|{
-        let mut p1_rad = p1[1].atan2(p1[0]);
-        let mut p2_rad = p2[1].atan2(p2[0]);
-
-        if p1_rad < 0.0 { p1_rad += std::f32::consts::PI * 2.0 }
-        if p2_rad < 0.0 { p2_rad += std::f32::consts::PI * 2.0 }
-
-        p1_rad.partial_cmp(&p2_rad).unwrap()
-    });
-
-    Matrix2x4::from_columns(&points)
-}
+pub mod compute_worker;
 
 fn main() {
     let center1 = Vector2::new(40.0, 1.0);
     let center2 = Vector2::new(1.0, 2.0);
     let radius = 6.0 / 0.2;
-    let quad = find_rectangle_points(center1, center2, radius);
+    let quad = compute_worker::find_rectangle_points(center1, center2, radius);
     println!("Sorted Quad: {}", quad);
 }
