18d1e4ae10fb3d3b1ea0e220176426ae96c776cc
Thorhian <thorhian@gmail.com>
2022-12-19 13:44:58 -0700

    Added russimp crate

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ pub mod compute_worker;
 
 use std::path::PathBuf;
 use nalgebra::Vector2;
-use clap::{Parser, Subcommand};
+use clap::Parser;
+use russimp::scene;
+use russimp::scene::Scene;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -24,4 +26,19 @@ fn main() {
     let radius = 6.0 / 0.2;
     let quad = compute_worker::find_rectangle_points(center1, center2, radius);
     println!("Sorted Quad: {}", quad);
+
+    let mut post_process_target = scene::PostProcessSteps::new();
+    let mut post_process_stock = scene::PostProcessSteps::new();
+    post_process_target.push(scene::PostProcess::ForceGenerateNormals);
+    post_process_stock.push(scene::PostProcess::ForceGenerateNormals);
+
+    let binding = cli.target_model.into_os_string();
+    let target_path = binding.to_str().unwrap();
+    let binding = cli.stock_model.into_os_string();
+    let stock_path = binding.to_str().unwrap();
+    let _target_scene = Scene::from_file(target_path, post_process_target).unwrap();
+    let stock_scene =  Scene::from_file(stock_path, post_process_stock).unwrap();
+
+    println!("Stock Vertices: {:?}", stock_scene.meshes[0].vertices);
+
 }
