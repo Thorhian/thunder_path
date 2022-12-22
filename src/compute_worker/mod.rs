df5825ee1f484d9645a222aa77559cfd996eb8f3
Thorhian <thorhian@gmail.com>
2022-12-22 13:00:19 -0700

    Bounding Box/Ortho Matrix Creation

--- src/compute_worker/mod.rs
+++ src/compute_worker/mod.rs
@@ -7,6 +7,76 @@ use nalgebra::Matrix2x4;
 use nalgebra::Vector2;
 use crate::job::Job;
 
+
+#[repr(C)]
+#[derive(Default, Copy, Clone, Zeroable, Pod)]
+struct CPUVertex {
+    position: [f32; 3],
+    color: [u32; 3]
+}
+
+vulkano::impl_vertex!(CPUVertex, position);
+
+pub fn process_job(job: Job) {
+    let (device , _queue) = additive_renderer::initialize_device();
+    let allocator = StandardMemoryAllocator::new_default(device.clone());
+
+    let target_model = job.target_mesh.clone();
+    let target_verts = target_model.vertices.iter();
+    let first_vert = target_model.vertices.first().unwrap();
+
+    let mut target_vert_buff: Vec<CPUVertex> = Vec::new();
+    let mut bounds: Vec<f32> = vec![first_vert.x, first_vert.x,
+                                    first_vert.y, first_vert.y,
+                                    first_vert.z, first_vert.z];
+    for vertex in target_verts {
+        let (x, y, z) = (vertex.x, vertex.y, vertex.z);
+
+        if x < bounds[0] {
+            bounds[0] = x;
+        }
+        if x > bounds[1] {
+            bounds[1] = x;
+        }
+        if y < bounds[2] {
+            bounds[2] = y;
+        }
+        if y > bounds[3] {
+            bounds[3] = y;
+        }
+        if z < bounds[5] {
+            bounds[5] = z;
+        }
+        if z > bounds[4] {
+            bounds[4] = z;
+        }
+
+
+        let vert = CPUVertex {
+            position: [x, y, z],
+            color: [0, 0, 255],
+        };
+
+        target_vert_buff.push(vert);
+    }
+    println!("Bounds: {:?}", bounds);
+
+    let ortho_matrix = nalgebra::Orthographic3::new(bounds[0], bounds[1],
+        bounds[2], bounds[3],
+        bounds[4], bounds[5]);
+
+    ortho_matrix.as_matrix().iter();
+
+    let _ex_buffer = CpuAccessibleBuffer::from_iter(
+        &allocator,
+        BufferUsage {
+            vertex_buffer: true,
+            ..Default::default()
+        }, 
+        false, 
+        target_vert_buff);
+}
+
 pub fn find_rectangle_points(
     center1: Vector2<f32>,
     center2: Vector2<f32>,
@@ -33,43 +103,4 @@ pub fn find_rectangle_points(
     });
 
     Matrix2x4::from_columns(&points)
-}
-
-#[repr(C)]
-#[derive(Default, Copy, Clone, Zeroable, Pod)]
-struct CPUVertex {
-    position: [f32; 3],
-    color: [u32; 3]
-}
-
-vulkano::impl_vertex!(CPUVertex, position);
-
-pub fn process_job(job: Job) {
-    let (device , _queue) = additive_renderer::initialize_device();
-    let allocator = StandardMemoryAllocator::new_default(device.clone());
-
-    let target_model = job.target_mesh.clone();
-    let target_verts = target_model.vertices.iter();
-
-    let mut target_vert_buff: Vec<CPUVertex> = Vec::new();
-    for vertex in target_verts {
-        let (x, y, z) = (vertex.x, vertex.y, vertex.z);
-
-        let vert = CPUVertex {
-            position: [x, y, z],
-            color: [0, 0, 255],
-        };
-
-        target_vert_buff.push(vert);
-    }
-
-
-    let _ex_buffer = CpuAccessibleBuffer::from_iter(
-        &allocator,
-        BufferUsage {
-            vertex_buffer: true,
-            ..Default::default()
-        }, 
-        false, 
-        target_vert_buff);
 }
